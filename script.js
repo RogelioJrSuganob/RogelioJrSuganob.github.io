b35ac7f4bbd45a39b574de018bcd3c67e491a33c
Rog <60377421+RogelioJrSuganob@users.noreply.github.com>
2026-04-09 13:20:54 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Simple smooth scroll
+document.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", e => {
+    if (link.hash !== "") {
+      e.preventDefault();
+      document.querySelector(link.hash).scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
